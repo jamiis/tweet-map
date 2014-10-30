fb73b82b2d559ffedbbc64a140399d586d1bdcd8
Jamis Johnson <jamismanwaring@gmail.com>
2014-10-30 13:36:24 -0400

    adds sample environment keys file

---
+++ server/config/env/keys.sample.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// Use for your api keys, secrets, etc. 
+// This file should not be tracked by git.
+// You will need to set these on your server.
+
+module.exports = {
+  aws: {
+    accessKey = '',
+    accessKeySecret = ''
+  },
+  twitter: {
+    consumerKey: '',
+    consumerSecret: '',
+    accessToken: '',
+    accessTokenSecret: ''
+  }
+};
